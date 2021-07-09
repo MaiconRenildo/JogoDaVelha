b233df4097f4c06a262f6a39ebf75f9d7d522fe2
Maicon Renildo <63758491+MaiconRenildo@users.noreply.github.com>
2021-07-09 00:20:26 -0300

    Remoção dos alertas

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ function finalizar(){
     (valores[2]==valores[5] && valores[2]==valores[8] && valores[2]!=null) 
     ){
     if((vez%2)==0){
-      setTimeout(function(){ alert("Vencedor: X"); }, 100);
+      //setTimeout(function(){ alert("Vencedor: X"); }, 100);
       win=1;
     }else{
-      setTimeout(function(){ alert("Vencedor: O"); }, 100);
+      //setTimeout(function(){ alert("Vencedor: O"); }, 100);
       win=2;
     }
   }else if(vez==8){
-    setTimeout(function(){ alert("EMPATE"); }, 100);
+    //setTimeout(function(){ alert("EMPATE"); }, 100);
   }
 }
 
